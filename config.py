122b1b15c47ca7b338974c03ba3d24c971786a82
Boxiang Zhang <48875216+Yveszhangbx@users.noreply.github.com>
2020-11-26 02:40:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#config for building the network
+MAXLEN=12
+D_MODEL= 256
+LAYER_NUM = 3
+HEAD_NUM = 4
+
+size_t=1093
+size_c=339
+size_i=52739
+
+#config for training
+BATCH_SIZE=256
+EPOCH = 20
+beta_1=0.9
+beta_2=0.98
+epsilon=1e-9
+
